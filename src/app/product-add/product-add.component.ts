6c1cefea54672f2932e4cabc688cbca5f04bfb24
Samuel Joshua <isamueljoshua@gmail.com>
2019-09-15 01:04:55 +0530

    submit form values to node server

--- src/app/product-add/product-add.component.ts
+++ src/app/product-add/product-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-add',
@@ -10,7 +11,7 @@ export class ProductAddComponent implements OnInit {
 
   angForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private ps: ProductsService) { 
     this.createForm();
   }
 
@@ -20,6 +21,10 @@ export class ProductAddComponent implements OnInit {
       ProductDescription: ['', Validators.required ],
       ProductPrice: ['', Validators.required ]
     });
+  }
+
+  addProduct(ProductName, ProductDescription, ProductPrice) {
+    this.ps.addProduct(ProductName, ProductDescription, ProductPrice);
   }
 
   ngOnInit() {
