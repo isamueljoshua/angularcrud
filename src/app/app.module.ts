3c729e05cbcfb45ed27965f93e9dffae5b5ba5b8
Samuel Joshua <isamueljoshua@gmail.com>
2019-09-15 00:35:33 +0530

    step10 create angular service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductGetComponent } from './product-get/product-get.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ProductsService } from './products.service';
 
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -25,7 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
