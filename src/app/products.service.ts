d4b575e8183f6612c98500fe836548541430e8ce
Samuel Joshua <isamueljoshua@gmail.com>
2019-09-15 14:08:28 +0530

    edit and update the fields

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -26,4 +26,22 @@ export class ProductsService {
            .http
            .get(`${this.uri}`);
   }
+
+  editProduct(id) {
+    return this
+            .http
+            .get(`${this.uri}/edit/${id}`);
+    }
+
+    updateProduct(ProductName, ProductDescription, ProductPrice, id) {
+      const obj = {
+        ProductName,
+        ProductDescription,
+        ProductPrice
+      };
+      this
+        .http
+        .post(`${this.uri}/update/${id}`, obj)
+        .subscribe(res => console.log('Done'));
+  }
 }
